2e0170c01c7fed4a576472b7f4812fcad2a6eaa3
bouz2004 <99121779+bouz2004@users.noreply.github.com>
2022-02-06 12:05:01 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+function color(){
+
+    document.getElementById('change').style.color= "blue"
+}
+function tt(){
+
+    document.getElementById('tt1').style.fontSize= "50px"
+}
+function aa(){
+    document.getElementById('aa').style.color="red"
+}
+function ee(){
+    document.getElementById('ee').style.color="green"
+}
+function yy(){
+    document.getElementById('yy').style.fontFamily="serif"
+}
+function ii(){
+    document.getElementById('ii').style.fontSize="50px"}
